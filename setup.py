61ce7f70d9e026de1eed4e1acfd4901a1e63271b
unknown <mike.heald@canonical.com>
2012-04-04 09:21:07 -0700

    Allowed owner field to be optional

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://launchpad.net/django-group-access',
     license='LGPLv3',
     keywords=['django', 'ownership', 'models'],
-    version="1.1.2",
+    version="1.1.3",
     classifiers=[
         "Development Status :: 4 - Beta",
         'Environment :: Web Environment',
