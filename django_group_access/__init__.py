b86da2e30305f5580ba5520587c1cb08b76098fd
Mike Heald <mike.heald@canonical.com>
2013-06-21 14:42:51 +0100

    Only decorate once

--- django_group_access/__init__.py
+++ django_group_access/__init__.py
@@ -178,12 +178,12 @@ def wrap_descriptor_get(func, do_wrap_get_query_set=False):
             return obj
         if hasattr(instance, '_access_control_meta'):
             obj._access_control_meta = instance._access_control_meta
-
         if do_wrap_get_query_set and hasattr(obj, 'get_query_set'):
-            obj.get_query_set = types.MethodType(
-                wrap_related_manager_get_query_set(
-                    obj.get_query_set), obj)
-
+            if obj.get_query_set.im_func.__name__ != 'get_query_set_wrapper':
+                # only wrap it once
+                obj.get_query_set = types.MethodType(
+                    wrap_related_manager_get_query_set(
+                        obj.get_query_set), obj)
         return obj
     return get_wrapper
 
