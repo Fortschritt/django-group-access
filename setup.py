61408365bc45a64889688f9ba0d63d623861062b
Mike Heald <mike.heald@canonical.com>
2013-06-24 12:10:52 +0100

    Bump to 1.1.14

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://launchpad.net/django-group-access',
     license='LGPLv3',
     keywords=['django', 'ownership', 'models'],
-    version="1.1.13",
+    version="1.1.14",
     classifiers=[
         "Development Status :: 4 - Beta",
         'Environment :: Web Environment',
